0851f48658bccc44045b86d79813647de0c5b4f9
diendh <diendh001@gmail.com>
2013-12-18 13:40:11 +0700

    creat app

---
+++ funyCamera/funyCamera/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  funyCamera
+//
+//  Created by Đỗ Hữu Điển on 12/18/13.
+//  Copyright (c) 2013 Đỗ Hữu Điển. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
